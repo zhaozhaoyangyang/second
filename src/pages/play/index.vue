ba0add4a45447b1a48e12e108cb523293a253a84
yanghao6688 <1512350723@qq.com>
2021-06-07 20:58:35 +0800

    播放器样式

--- src/pages/play/index.vue
+++ src/pages/play/index.vue
@@ -1,35 +1,34 @@
 <template>
-    <div class=''>播放</div>
+  <div class="player">
+    <NormarlPlayer v-model="play" :progress='progress'/>
+    <MinePlayer v-model="play" :progress='progress' />
+  </div>
 </template>
 
 <script>
+import MinePlayer from "./miniplayer";
+import NormarlPlayer from "./normalplayer";
 export default {
-    
-    components: {},
-    data() {
-        
-        return {
-            
-        };
-    },
-    computed: {},
-    watch: {},
-    
-    methods: {
-        
-    },
-    created() {
-        
-    },
-    mounted() {
-        
-    },
-    beforeCreate() {}, 
-    beforeMount() {},
-    beforeUpdate() {}, 
-    updated() {}, 
-    }
+  components: {
+    MinePlayer,
+    NormarlPlayer,
+  },
+  data() {
+    return {
+      play: false,
+      progress:0.4
+    };
+  },
+  computed: {},
+  watch: {},
+
+  methods: {},
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+};
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>
--- src/store/count/mutations.js
+++ src/store/count/mutations.js
@@ -1,5 +1,10 @@
 export default {
- radioIdList(state,id){
-    state.radioId=id
- }
+  radioIdList(state, id) {
+    state.radioId = id;
+  },
+   setnormalPlayerDom(state, payload) {
+      console.log(payload);
+      
+      state.normalplayerdom = payload;
+  },
 };
