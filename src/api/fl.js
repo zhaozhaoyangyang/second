52a58703798c2a4726652622a02001cda7141cd9
花海 <746493056@qq.com>
2021-06-07 22:02:42 +0800

    dl

--- src/api/fl.js
+++ src/api/fl.js
@@ -1,5 +1,21 @@
 import { post } from "../utils/request";
 //歌手列表
+<<<<<<< HEAD
 export const getSinger = () => post("/top/artists");
 //获取歌手歌曲
 export const reqsings = (id) => post("/artist/songs?id=" + id);
+=======
+export const getSinger = () => post('/top/artists')
+//获取歌手热门50歌曲
+export const reqHotSings = (id) =>post('/artist/top/song',id)
+//获取歌手详情
+export const reqGetSinger = (id) =>post('/artist/detail',id)
+//获取歌手的mv
+export const reqGetMv = (id) =>post('/artist/mv',id)
+//获取歌手专辑
+export const reqZj = (id) =>post('/artist/album',id)
+//歌手描述
+export const reqMs = (id) =>post('/artist/desc',id)
+//歌单
+export const reqGd = (data) =>post('/personalized',data)
+>>>>>>> c5d4550fb93d8ca56f6f1263b0122c95f1e4d819
---
+++ src/components/back.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="">
+    <div class="a">
+      <slot name="left"><van-icon name="arrow-left"  @click="btn"/></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+
+  methods: {
+      btn(){
+          this.$router.back()
+      }
+  },
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+};
+</script>
+<style scoped>
+.a {
+  width: 100%;
+  height: 40px;
+  /* background-color: skyblue; */
+  line-height: 40px;
+}
+
+</style>
