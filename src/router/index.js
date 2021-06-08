1d9587537ffc0c28c92e6406dd978aeb6c21470f
赵杨 <2296505513@qq.com>
2021-06-08 15:51:02 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,22 @@ import Vue from "vue";
 
 import VueRouter from "vue-router";
 
-
-// const originalPush = VueRouter.prototype.push;
-// const originalReplace = VueRouter.prototype.replace;
-// //push
-// VueRouter.prototype.push = function push(location, onResolve, onReject) {
-//   if (onResolve || onReject)
-//     return originalPush.call(this, location, onResolve, onReject);
-//   return originalPush.call(this, location).catch(err => err);
-// };
-// //replace
-// VueRouter.prototype.replace = function push(location, onResolve, onReject) {
-//   if (onResolve || onReject)
-//     return originalReplace.call(this, location, onResolve, onReject);
-//   return originalReplace.call(this, location).catch(err => err);
-// };
+// 这个错误是vue-router内部错误,导致导航跳转问题,、
+// 往同一地址跳转时会报错的情况push和replace 都会导致这个情况的发生。
+const originalPush = VueRouter.prototype.push;
+const originalReplace = VueRouter.prototype.replace;
+//push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch(err => err);
+};
+//replace
+VueRouter.prototype.replace = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalReplace.call(this, location, onResolve, onReject);
+  return originalReplace.call(this, location).catch(err => err);
+};
 
 
 
