2f3a6618203c3b959039ce4afa401ee1b3c8af09
yanghao6688 <1512350723@qq.com>
2021-06-08 08:25:27 +0800

    store

--- src/store/count/getters.js
+++ src/store/count/getters.js
@@ -1,3 +1,9 @@
 export default {
- 
+    currentSong(state) {
+        if (state.currentIndex >= 0 && state.currentIndex < state.sequencePlayList.length) {
+            return state.sequencePlayList[state.currentIndex]
+        } else {
+            return {}
+        }
+ }
 };
--- src/store/count/state.js
+++ src/store/count/state.js
@@ -5,7 +5,7 @@ const playMode = {
   random: 2, //随机
 };
 export default {
-  radioId: [],
+  // radioId: [],
 
   // play组件共享数据
   fullScreen: false, //是否显示大播放器
@@ -16,3 +16,26 @@ export default {
   currentIndex: -1, //当前播放音乐在歌曲列表的下标
   currentSong: {}, //当前歌曲信息
 };
+/*  需要上传 
+currentSong  格式
+{
+  al:{
+  name,//歌曲名
+  picUrl,//歌曲图片
+  },
+  ar,作者
+  id,歌曲id
+}
+
+过滤
+xxx.map(item=>{
+  id:item.id,
+  name:item.name,
+  ar:item.ar.map(v=>v.name).join('/),
+  al:{
+    name:item.al.name,
+    picUrl:item.al.picUrl
+  }
+})
+
+*/
